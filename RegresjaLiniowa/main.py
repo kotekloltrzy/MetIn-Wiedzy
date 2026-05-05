8f2b93281ea18f2ca44b93593a5ad3334af9b6bd
Michał Broda <147035736+kotekloltrzy@users.noreply.github.com>
2026-05-05 12:37:22 +0200

    Add files via upload

--- RegresjaLiniowa/main.py
+++ RegresjaLiniowa/main.py
@@ -1,6 +1,18 @@
+import matplotlib
 import matplotlib.pyplot as plt
-x = [1, 2, 3, 4, 5]
-y = [4, 6, 9, 11, 18]
+import numpy as np
+matplotlib.use('TkAgg')
+
+
+def wczytaj_dane(plik):
+    temp1 = []
+    temp2 = []
+    with open(plik, "r") as f:
+        for line in f:
+            a, b = map(int, line.split())
+            temp1.append(a)
+            temp2.append(b)
+    return temp1, temp2
 
 
 def srednia_aryt(zbior):
@@ -8,10 +20,6 @@ def srednia_aryt(zbior):
     for i in zbior:
         wynik = wynik + i
     return wynik / len(zbior)
-
-
-Mx = srednia_aryt(x)
-My = srednia_aryt(y)
 
 
 def odchyl_stand(zbior, srednia):
@@ -23,13 +31,8 @@ def odchyl_stand(zbior, srednia):
     return round(wynik, 2)
 
 
-Sx = odchyl_stand(x, Mx)
-Sy = odchyl_stand(y, My)
-
-
-def korelacja_Pearsona(zbiorx, zbiory):
+def korelacja_pearsona(zbiorx, zbiory):
     licznik = 0
-    mianownik = 0
     sumax = sum(zbiorx)
     sumay = sum(zbiory)
     sumaxk = 0
@@ -48,20 +51,43 @@ def korelacja_Pearsona(zbiorx, zbiory):
     return round(licznik/mianownik, 2)
 
 
-r = korelacja_Pearsona(x, y)
-
-
 def naj_pas_lin(Mx, My, Sx, Sy, r):
     b = round(r * (Sy/Sx), 3)
     a = round(My - (b * Mx), 3)
     return a, b
 
 
-a, b = naj_pas_lin(Mx, My, Sx, Sy, r)
-
-
-def funkcja(a, b):
-
+def wykres(dane_a, dane_b, dane_x, dane_y):
+    temp_x = np.linspace(0, 5, 100)
+    temp_y = dane_b * temp_x + dane_a
     plt.title("Regresja liniowa")
     plt.xlabel("Wartości X")
     plt.ylabel("Wartości Y")
+    plt.plot(temp_x, temp_y, label=f'y = {dane_b}x + {dane_a}')
+    plt.scatter(dane_x, dane_y, label="Wartości Niezależne", color='red')
+    plt.legend()
+    plt.show()
+
+
+def przewidywanie(a, b, nowe_x):
+    y_pred = b * nowe_x + a
+    return round(y_pred, 2)
+
+
+x, y = wczytaj_dane("dane.txt")
+
+
+def uruchomienie_funckji(dane_x, dane_y, nowe_x=None):
+    Mx = srednia_aryt(dane_x)
+    My = srednia_aryt(dane_y)
+    Sx = odchyl_stand(dane_x, Mx)
+    Sy = odchyl_stand(dane_y, My)
+    r = korelacja_pearsona(dane_x, dane_y)
+    a, b = naj_pas_lin(Mx, My, Sx, Sy, r)
+    wykres(a, b, dane_x, dane_y)
+    if nowe_x is not None:
+        y_pred = przewidywanie(a, b, nowe_x)
+        print(f"Predykcja dla x = {nowe_x}: y = {y_pred}")
+
+
+uruchomienie_funckji(x, y, nowe_x=6)
